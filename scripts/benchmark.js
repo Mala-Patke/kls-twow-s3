fa6e08351020c761af0faefd4defafbb8e6e531f
Ali Shahid <hobocirclemeditation@gmail.com>
2022-01-06 10:35:51 -0800

    Use console.time for benchmark

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,10 +1,13 @@
 const db = require('../database/dbwrapper');
 
 (async function(){
-    console.time('t');
+    console.time('Read');
     let config = await db.getConfig();
-    console.timeLog('t', 'Read');
+    console.timeLog('Read');
+
+    console.time('Write')
     db.handleConfigUpdate(config)
-    console.timeLog('t', 'Write');
+    console.timeLog('Write');
+    
     process.exit();
 })();
