4c39f5cc114bb1edb66ede6438bbc35d03ee284b
Ali Shahid <hobocirclemeditation@gmail.com>
2022-01-14 11:50:55 -0800

    Fix typo

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,7 @@ router.get('/callback', async (req, res) => {
     if(!req.query.email.endsWith('khanlabschool.org')) return res.sendStatus(403);
 
     let firstname = req.query.user.username.split(" ")[0];
-    if(Object.keys(rename).includes(firstname)) req.query.username.replace(firstname, rename[firstname]);
+    if(Object.keys(rename).includes(firstname)) req.query.user.username.replace(firstname, rename[firstname]);
     let userdata = await User.register(req.query.user);
     req.session.user = userdata;
 
