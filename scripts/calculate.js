0ea23966d25fe99b80c6edbf8a283d16b34850ed
Ali Shahid <hobocirclemeditation@gmail.com>
2022-01-27 09:40:05 -0800

    Fix calculate script

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -2,9 +2,9 @@ const { getVotes, getUsers, getResponses } = require('../database/dbwrapper');
 
 if(!process.argv.slice(2).length) return console.log("Specify a round");
 
-getVotes(process.argv.slice(2)).then(votes => {
+getVotes(process.argv.slice(2).join(" ")).then(votes => {
     getUsers().then(users => {
-        getResponses(process.argv.slice(2)).then(responses => {
+        getResponses(process.argv.slice(2).join(" ")).then(responses => {
             console.log(require('../lib/calculate')(votes, users, responses));
             process.exit();
         })
